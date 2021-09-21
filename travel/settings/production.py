92bff23d5ca9a85a48c8cbbf7c03fde2659a9fe7
archdim1 <archibelousov@gmail.com>
2021-09-21 22:55:55 +0300

    Добавил фикстуры

--- travel/settings/production.py
+++ travel/settings/production.py
@@ -139,10 +139,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
-
-
 STATIC_ROOT = BASE_DIR.joinpath('staticfiles')

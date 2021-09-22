004f888dc8135735925c9a407504a45a658b3f08
archdim1 <archibelousov@gmail.com>
2021-09-22 12:09:00 +0300

    Финал

--- trains/models.py
+++ trains/models.py
@@ -5,19 +5,21 @@ from cities.models import City
 
 
 class Train(models.Model):
-    name = models.CharField(max_length=50,
-                            unique=True,
+    name = models.CharField(max_length=50, unique=True,
                             verbose_name='Номер поезда')
     travel_time = models.PositiveSmallIntegerField(verbose_name='Время в пути')
     from_city = models.ForeignKey(City, on_delete=models.CASCADE,
+                                  # null=True, blank=True,
                                   related_name='from_city_set',
-                                  verbose_name='Из какого города')
-    to_city = models.ForeignKey(City, on_delete=models.CASCADE,
-                                  related_name='to_city_set',
-                                  verbose_name='В какой город')
+                                  verbose_name='Из какого города'
+                                  )
+    to_city = models.ForeignKey('cities.City', on_delete=models.CASCADE,
+                                related_name='to_city_set',
+                                verbose_name='В какой город'
+                                )
 
     def __str__(self):
-        return f'Поезд № {self.name} из города {self.from_city}'
+        return f'Поезд №{self.name} из города {self.from_city}'
 
     class Meta:
         verbose_name = 'Поезд'
@@ -27,9 +29,9 @@ class Train(models.Model):
     def clean(self):
         if self.from_city == self.to_city:
             raise ValidationError('Изменить город прибытия')
-        qs = Train.objects.filter(from_city=self.to_city,
-                                  to_city=self.to_city,
-                                  travel_time=self.travel_time).exclude(pk=self.pk)
+        qs = Train.objects.filter(
+            from_city=self.from_city, to_city=self.to_city,
+            travel_time=self.travel_time).exclude(pk=self.pk)
         # Train == self.__class__
         if qs.exists():
             raise ValidationError('Измените время в пути')
